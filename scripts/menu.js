98160542d359a39516e79b5da8db9bb42c18398a
aleksander <aleksanderjozwik@elektronik.edu.pl>
2022-12-25 23:56:29 +0100

    Back to the top button fix

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,39 +1,48 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const toc = document.querySelector('#TOC');
-    const tocMenu = toc.cloneNode(true);
-    tocMenu.classList.add('toc-menu');
-    tocMenu.classList.remove('toc');
-    tocMenu.classList.add('disabled');
-    document.body.appendChild(tocMenu);
-    
-    tocMenu.addEventListener('click', (e) => {
-        if(e.target.tagName === 'A'){
-            tocMenu.classList.add('disabled');
-        }
-    });
+$("document").ready(() => {
+  const toc = $('#TOC')[0];
+  const tocMenu = toc.cloneNode(true);
+  tocMenu.classList.add("toc-menu");
+  tocMenu.classList.remove("toc");
+  tocMenu.classList.add("disabled");
+  document.body.appendChild(tocMenu);
 
-    const menu = document.createElement('div');
-    menu.classList.add('menu');
-    document.body.appendChild(menu);
+  tocMenu.addEventListener("click", (e) => {
+    if (e.target.tagName === "A") {
+      tocMenu.classList.add("disabled");
+    }
+  });
 
-    const scrollBtn = document.createElement('a');
-    scrollBtn.innerHTML = '<i class="fas fa-arrow-up"></i>';
-    scrollBtn.href = '#';
-    scrollBtn.classList.add('scroll-button');
-    menu.appendChild(scrollBtn);
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
+  document.body.appendChild(menu);
 
-    const tocBtn = document.createElement('a');
-    tocBtn.innerHTML = '<i class="fas fa-list"></i>';
-    tocBtn.classList.add('toc-button');
-    menu.appendChild(tocBtn);
+  const scrollBtn = document.createElement("a");
+  scrollBtn.innerHTML = '<i class="fas fa-arrow-up"></i>';
+  scrollBtn.href = "#";
+  scrollBtn.classList.add("scroll-button");
+  scrollBtn.classList.add("disabled");
+  menu.appendChild(scrollBtn);
 
-    tocBtn.addEventListener('click', () => {
-        tocMenu.classList.toggle('disabled');
-    });
+  const tocBtn = document.createElement("a");
+  tocBtn.innerHTML = '<i class="fas fa-list"></i>';
+  tocBtn.classList.add("toc-button");
+  menu.appendChild(tocBtn);
 
-    document.addEventListener('click', (e) => {
-        if(!tocMenu.contains(e.target) && !tocBtn.contains(e.target)){
-            tocMenu.classList.add('disabled');
-        }
-    });
- }, false);
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      $(".scroll-button").removeClass("disabled");
+    } else {
+      $(".scroll-button").addClass("disabled");
+    }
+  });
+
+  tocBtn.addEventListener("click", () => {
+    tocMenu.classList.toggle("disabled");
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!tocMenu.contains(e.target) && !tocBtn.contains(e.target)) {
+      tocMenu.classList.add("disabled");
+    }
+  });
+});
